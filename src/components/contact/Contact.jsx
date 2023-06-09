98011294acd64e0a6bd23b5825ab318f91299b3c
erina92 <erikamiglietta92@gmail.com>
2023-06-09 11:32:13 +0200

    added emailjs to send email directly from the form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import "./contact.css";
 
 const Contact = () => {
+  const form = useRef();
+  const [message, setMessage] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_tzdyrzd",
+        "template_xt7yofd",
+        form.current,
+        "cNSIlVWEWDInmdpbi"
+      )
+      .then(
+        (result) => {
+          setMessage("Email sent successfully!");
+        },
+        (error) => {
+          setMessage("Error sending email!");
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <section className="contact container section" id="contact">
       <h2 className="section__title">Get In Touch</h2>
@@ -14,13 +39,15 @@ const Contact = () => {
           </p>
         </div>
 
-        <form className="contact__form">
+        <form className="contact__form" ref={form} onSubmit={sendEmail}>
           <div className="contact__form-group">
             <div className="contact__form-div">
               <input
                 className="contact__form-input"
                 type="text"
                 placeholder="Insert your name"
+                name="user_name"
+                required
               />
             </div>
 
@@ -29,6 +56,8 @@ const Contact = () => {
                 className="contact__form-input"
                 type="email"
                 placeholder="Insert your email"
+                name="user_email"
+                required
               />
             </div>
           </div>
@@ -38,11 +67,13 @@ const Contact = () => {
               className="contact__form-input"
               type="text"
               placeholder="Insert your subject"
+              name="subject"
+              required
             />
           </div>
           <div className="contact__form-div contact__form-area">
             <textarea
-              name=""
+              name="message"
               id=""
               cols="30"
               rows="10"
@@ -52,6 +83,7 @@ const Contact = () => {
           </div>
 
           <button className="btn">Send Message</button>
+          {message && <p className="message__sent">🎉{message}🎉</p>}
         </form>
       </div>
     </section>
