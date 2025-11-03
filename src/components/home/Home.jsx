ed42bf434ebbe91297fb5155d53c1be2196d3adf
erina92 <erikamiglietta92@gmail.com>
2025-11-03 08:47:25 +0100

    updating working info

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -20,10 +20,10 @@ const Home = () => {
         </h1>
         <span className="home__education">
           {isItalian
-            ? "Sono una traduttrice e una sviluppatrice Front-End"
+            ? "Sono una traduttrice e una sviluppatrice Front-End, Infra Transformation Analyst in Accenture dal 2024"
             : isFrench
-            ? "Je suis traductrice et développeuse Front-End"
-            : "I am a Translator and a Front-End Developer"}
+            ? "Je suis traductrice et développeuse Front-End, Infra Transformation Analyst in Accenture depuis 2024"
+            : "I am a Translator and a Front-End Developer, Infra Transformation Analyst in Accenture since 2024"}
         </span>
         <br />
         <span className="home__location">
